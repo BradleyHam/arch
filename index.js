ea3c2cc5db42cdd42e4f7e985f7a2c6d55135665
bradleyham <bradleyjamesham@gmail.com>
2020-11-17 21:53:04 +1300

    parcel added and header finished

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import './styles/style.scss';
+import './styles/header.scss';
+import './styles/hamburger.css';
+
+var hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('is-active')
+    }
+)
