353c8d3d2a76d248b0822b7c287e49b8c446f82e
bradleyham <bradleyjamesham@gmail.com>
2020-11-20 20:54:27 +1300

    hero content selector functionality finished

--- dist/archCode.e31bb0bc.js
+++ dist/archCode.e31bb0bc.js
@@ -194,7 +194,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./hamburger.css":"styles/hamburger.css","./../images/home/mobile/image-hero-paramour.jpg":[["image-hero-paramour.4cf81781.jpg","images/home/mobile/image-hero-paramour.jpg"],"images/home/mobile/image-hero-paramour.jpg"],"./../images/home/tablet/image-hero-paramour.jpg":[["image-hero-paramour.93d40ec1.jpg","images/home/tablet/image-hero-paramour.jpg"],"images/home/tablet/image-hero-paramour.jpg"],"./../images/home/desktop/image-hero-paramour.jpg":[["image-hero-paramour.5caf8b45.jpg","images/home/desktop/image-hero-paramour.jpg"],"images/home/desktop/image-hero-paramour.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./hamburger.css":"styles/hamburger.css","./../images/home/mobile/image-hero-paramour.jpg":[["image-hero-paramour.4cf81781.jpg","images/home/mobile/image-hero-paramour.jpg"],"images/home/mobile/image-hero-paramour.jpg"],"./../images/home/tablet/image-hero-paramour.jpg":[["image-hero-paramour.93d40ec1.jpg","images/home/tablet/image-hero-paramour.jpg"],"images/home/tablet/image-hero-paramour.jpg"],"./../images/home/desktop/image-hero-paramour.jpg":[["image-hero-paramour.5caf8b45.jpg","images/home/desktop/image-hero-paramour.jpg"],"images/home/desktop/image-hero-paramour.jpg"],"./../images/home/desktop/image-hero-seraph.jpg":[["image-hero-seraph.28bb780a.jpg","images/home/desktop/image-hero-seraph.jpg"],"images/home/desktop/image-hero-seraph.jpg"],"./../images/home/desktop/image-hero-federal.jpg":[["image-hero-federal.e4822bb1.jpg","images/home/desktop/image-hero-federal.jpg"],"images/home/desktop/image-hero-federal.jpg"],"./../images/home/desktop/image-hero-trinity.jpg":[["image-hero-trinity.dbd0a5ba.jpg","images/home/desktop/image-hero-trinity.jpg"],"images/home/desktop/image-hero-trinity.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./styles/style.scss");
@@ -202,15 +202,41 @@ require("./styles/style.scss");
 var hamburger = document.querySelector('.hamburger');
 var contentSelectorList = document.querySelectorAll('.hero__content-selector div');
 var contentSelectorArray = Array.from(contentSelectorList);
+var heroElement = document.getElementById('hero');
+var heroHeading = document.getElementById('hero-h1');
+var heroText = document.getElementById('hero-p');
+console.log(heroText);
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('is-active');
 });
+
+function handleContentSelect(index) {
+  if (index === '1') {
+    heroElement.className = 'hero one-active';
+    heroHeading = 'Project Paramour';
+    heroText.innerText = 'Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.';
+  } else if (index === '2') {
+    heroElement.className = 'hero two-active';
+    heroHeading.innerText = 'Seraph Station';
+    heroText.innerText = 'The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.';
+  } else if (index === '3') {
+    heroElement.className = 'hero three-active';
+    heroHeading.innerText = 'Federal II Tower';
+    heroText.innerText = 'A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.';
+  } else {
+    heroElement.className = 'hero four-active';
+    heroHeading.innerText = 'Trinity Bank Tower';
+    heroText.innerText = 'Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.';
+  }
+}
+
 contentSelectorArray.forEach(function (item) {
   item.addEventListener('click', function (e) {
     contentSelectorArray.forEach(function (item) {
       item.classList.remove('active');
     });
     e.target.classList.add('active');
+    handleContentSelect(e.target.dataset.indexNumber);
   });
 });
 },{"./styles/style.scss":"styles/style.scss"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -241,7 +267,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54761" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58077" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -3,17 +3,43 @@ import './styles/style.scss';
 var hamburger = document.querySelector('.hamburger');
 var contentSelectorList = document.querySelectorAll('.hero__content-selector div');
 var contentSelectorArray = Array.from(contentSelectorList);
+var heroElement = document.getElementById('hero')
+var heroHeading = document.getElementById('hero-h1');
+var heroText = document.getElementById('hero-p');
+console.log(heroText)
 
 hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('is-active')
     }
 )
 
+function handleContentSelect(index){
+    if(index === '1'){
+        heroElement.className = 'hero one-active';
+        heroHeading = 'Project Paramour';
+        heroText.innerText ='Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.';
+    }else if(index === '2'){
+        heroElement.className = 'hero two-active';
+        heroHeading.innerText = 'Seraph Station';
+        heroText.innerText = 'The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.';
+    }else if(index === '3'){
+        heroElement.className = 'hero three-active'
+        heroHeading.innerText = 'Federal II Tower';
+        heroText.innerText = 'A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.';
+    }else{
+        heroElement.className = 'hero four-active';
+        heroHeading.innerText = 'Trinity Bank Tower';
+        heroText.innerText = 'Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.';
+
+    }
+}
+
 contentSelectorArray.forEach((item)=>{
     item.addEventListener('click', (e)=>{
         contentSelectorArray.forEach((item)=>{
             item.classList.remove('active')
         })
-        e.target.classList.add('active')
+        e.target.classList.add('active');
+        handleContentSelect(e.target.dataset.indexNumber);
     })
 })
